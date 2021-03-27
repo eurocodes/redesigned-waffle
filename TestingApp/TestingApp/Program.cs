4c0c5b3ef2d2a32a8a6352d3388f681bce0f41cd
Ugo Umeoke <laryhug209@yahoo.com>
2021-03-27 19:07:39 +0100

    First commit to test

---
+++ TestingApp/TestingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestingApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter salary");
+            string input = Console.ReadLine();
+            double salary = Convert.ToDouble(input);
+            double tax;
+
+            if (salary <= 8350)
+            {
+                tax = 8350 * .10;
+            }
+            else if (salary <= 33950)
+            {
+                tax = (8350 * .10) + ((salary - 8350) * .15);
+            }
+            else
+            {
+                tax = (8350 * .10) + ((salary - 8350) * .15) + ((salary - 33950) * .25);
+            }
+
+            Console.WriteLine("Tax payable is {0}", tax);
+
+            Console.ReadKey();
+        }
+    }
+}
